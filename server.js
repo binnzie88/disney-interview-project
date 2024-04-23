14aed56ae5239ec797c6f5a0566d6f2feef39f57
Emma Binns <ebinns88@gmail.com>
2024-04-23 15:34:53 -0700

    fix port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var app = express();
-const port = 5000
+const port = process.env.PORT || 5000;
 const path = require('path');
 const axios = require('axios');
 
